cb92eb14fc95c4dd3c91fb5db0a71140b21222d3
Umapathi2004 <umapathi1014@gmail.com>
2024-07-29 20:27:09 +0530

    changed

--- fs.js
+++ fs.js
@@ -1,9 +1,14 @@
-const http = require("http");
-const server = http.createServer((req,res)=>{
-   console.log("hi");
-   if(req.url=="/about"){
-    res.write("hi");
-    res.end();
-   }
-})
-server.listen(5000);
+const {readFile} = require("fs");
+const getText = (d) =>{
+  readFile(d,"utf8",(err,result)=>{
+    if(err){
+      console.log(err);
+      return;
+    }
+     console.log(result);
+  });
+}
+const first = async ()=>{
+   const data = await getText('./first.txt');
+}
+first();
